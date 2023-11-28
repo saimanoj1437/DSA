85ad4dfcb8596fe454a60c2b18f4208a6c7c7120
saimanoj1437 <130078620+saimanoj1437@users.noreply.github.com>
2023-11-28 22:13:36 +0530

    Time: 504 ms (94.43%), Space: 41.6 MB (55.72%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,26 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
-        fast = head
-        slow = head
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
         
-        # find the middle (slow)
+        slow,fast = head,head
+        
         while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
             
-        # reverse second half
+            slow = slow.next
+            fast = fast.next.next
+            
         prev = None
+        
         while slow:
+            
             tmp = slow.next
             slow.next = prev
             prev = slow
             slow = tmp
+         
+        left,right = head,prev
         
-        # check palindrome
-        left, right = head, prev
         while right:
+            
             if left.val != right.val:
+                
                 return False
             left = left.next
             right = right.next
+            
         return True
+            
+            
+        
